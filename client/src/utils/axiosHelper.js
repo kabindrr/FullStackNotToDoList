6bab7df18a16f910951826cb5a8e4dbfb1b3f8dd
kabindrr <chaina_kabindra@yahoo.com>
2024-04-23 09:00:33 +1000

    First Commit

---
+++ client/src/utils/axiosHelper.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+const apiEP = " http://localhost:8000/api/v1/tasks";
+
+export const postNewTask = async (taskObj) => {
+  try {
+    const { data } = await axios.post(apiEP, taskObj);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+export const getAllTasks = async () => {
+  try {
+    const { data } = await axios.get(apiEP);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+export const deleteTasks = async (_id) => {
+  try {
+    const { data } = await axios.delete(apiEP + "/" + _id);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+export const updateTask = async (obj) => {
+  try {
+    const { data } = await axios.patch(apiEP, obj);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
